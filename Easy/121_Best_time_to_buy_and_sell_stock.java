50d0289e5ebfcf76e43fc25fb5df1a132078713f
Sagar D <sagardevaraj07@gmail.com>
2025-09-03 13:39:47 +0530

    Update 121_Best_time_to_buy_and_sell_stock.java

--- Easy/121_Best_time_to_buy_and_sell_stock.java
+++ Easy/121_Best_time_to_buy_and_sell_stock.java
@@ -1,13 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int min = prices[0];
-        int profit = 0;
-        for(int i = 0; i < prices.length;i++){
-            if(prices[i] < min){
+        int profit =0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]<=min){
                 min = prices[i];
+                continue;
             }
-            profit = Math.max(profit,prices[i] - min);
+            profit=Math.max(profit,prices[i]-min);
         }
         return profit;
     }
-} 
+}
