2401d06c877190f78da6d63c015b3b6160f36f76
Sagar D <sagardevaraj07@gmail.com>
2025-09-03 13:43:57 +0530

    Update 238_product_of_array_except_self.java

--- Medium/238_product_of_array_except_self.java
+++ Medium/238_product_of_array_except_self.java
@@ -1,13 +1,14 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] result = new int[nums.length];
-        Arrays.fill(result,1);
-        int pre = 1,post = 1;
-        for(int i = 0;i < nums.length; i++){
+        int n  = nums.length;
+        int[] result = new int[n];
+        int pre = 1;
+        int post = 1;
+        for(int i = 0;i < n; i++){
             result[i] = pre;
             pre = nums[i] * pre;
         }
-        for(int i = nums.length-1; i >= 0; i--){
+        for(int i = n-1; i >= 0; i--){
             result[i] = result[i]*post;
             post = post * nums[i];
         }
