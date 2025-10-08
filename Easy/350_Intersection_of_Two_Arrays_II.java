c27280817d4a5a7594f7999d9076b77c4eb024be
Sagar D <sagardevaraj07@gmail.com>
2025-10-08 14:14:20 +0530

    Create 350_Intersection_of_Two_Arrays_II.java

---
+++ Easy/350_Intersection_of_Two_Arrays_II.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int n : nums1){
+            int freq = map.getOrDefault(n,0);
+            map.put(n,freq+1);
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i : nums2){
+            if(map.get(i) != null && map.get(i) > 0){
+                list.add(i);
+                map.put(i,map.get(i) - 1);
+            }
+        }
+        int [] ans = new int[list.size()];
+        for(int i = 0; i < list.size();i++){
+            ans[i]=list.get(i);
+        }
+        return ans;
+    }
+}
